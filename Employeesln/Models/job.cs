eb7f8b40d50d0ea0517324e144e48b8410926bd7
Rehihon <richard.edrent@gmail.com>
2026-09-16 15:42:05 +0100

    Add readme to the GigHub

---
+++ Employeesln/Models/job.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace EmployeeslnModel
+{
+    public class Jobs
+    {
+        public int JobID { get; set; }
+        public string? Job_Title { get; set; }
+        public decimal Min_salary { get; set; }
+        public decimal Max_Salary { get; set; }
+    }
+}
